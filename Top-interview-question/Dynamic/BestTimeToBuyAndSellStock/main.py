972cad98ab3543610af6b03cbc9d5a301cf511d5
dimadickx <dmitry.sorokin.mail@gmail.com>
2021-12-14 21:00:18 +0500

    add bestTimeToBuySell

--- Top-interview-question/Dynamic/BestTimeToBuyAndSellStock/main.py
+++ Top-interview-question/Dynamic/BestTimeToBuyAndSellStock/main.py
@@ -1,6 +1,15 @@
-from typing import List
-
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        min_price = float('inf')
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            profit = prices[i] - min_price
+            if max_profit < profit:
+                max_profit = profit
+        return max_profit
+
+
+if __name__ == '__main__':
+    pass
