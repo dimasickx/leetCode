bbdd0f810f995bc28ab5f45363e522958e33e815
dimasickx <56298445+dimasickx@users.noreply.github.com>
2022-08-02 16:51:53 +0300

    Delete low-top-interview-question directory

--- low-top-interview-question/Arrays/5SingleNumber/XORVersion.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        xor = 0
-        for i in nums:
-            xor ^= i
-        return xor
--- low-top-interview-question/Arrays/7PlusOne/main.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        pass
